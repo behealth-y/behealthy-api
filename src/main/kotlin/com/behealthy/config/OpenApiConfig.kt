133464fe780cade8fd44921992662289d1f46f96
lowelllll <yejin2651@naver.com>
2023-01-27 22:57:25 +0900

    docs: swagger SecuritySchema 추가

--- src/main/kotlin/com/behealthy/config/OpenApiConfig.kt
+++ src/main/kotlin/com/behealthy/config/OpenApiConfig.kt
@@ -1,8 +1,11 @@
 package com.behealthy.config
 
+import com.behealthy.domain.auth.AuthConstants.AUTHENTICATION_HEADER_NAME
 import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
 import org.springframework.boot.info.BuildProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -13,11 +16,21 @@ class OpenApiConfig {
     @Bean
     fun openAPI(buildProperties: BuildProperties): OpenAPI? {
         return OpenAPI()
-            .components(Components())
+            .components(
+                Components().addSecuritySchemes(
+                    AUTHENTICATION_HEADER_NAME,
+                    SecurityScheme()
+                        .name(AUTHENTICATION_HEADER_NAME)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("Bearer")
+                        .bearerFormat("JWT")
+                )
+            )
             .info(
                 Info()
                     .title("behealthy")
                     .version(buildProperties.version)
             )
+            .addSecurityItem(SecurityRequirement().addList(AUTHENTICATION_HEADER_NAME))
     }
 }
