5f0850d6c587d4661165266b7a66dfb240f1e5ec
lowelllll <yejin2651@naver.com>
2023-02-05 21:20:38 +0900

    feat: 운동 기록 수정 API

--- src/main/kotlin/com/behealthy/domain/workoutlog/service/WorkoutLogService.kt
+++ src/main/kotlin/com/behealthy/domain/workoutlog/service/WorkoutLogService.kt
@@ -1,8 +1,12 @@
 package com.behealthy.domain.workoutlog.service
 
 import com.behealthy.domain.workoutlog.dto.WorkoutLogDto
+import com.behealthy.domain.workoutlog.entity.WorkoutLogEntity
 import com.behealthy.domain.workoutlog.repository.WorkoutLogRepository
+import com.behealthy.domain.workoutlog.vo.WorkoutLog
+import com.behealthy.exception.WorkoutLogException
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class WorkoutLogService(private val workoutLogRepository: WorkoutLogRepository) {
@@ -10,4 +14,16 @@ class WorkoutLogService(private val workoutLogRepository: WorkoutLogRepository) 
     fun create(workoutLogDto: WorkoutLogDto) {
         workoutLogRepository.save(workoutLogDto.toWorkoutLogEntity())
     }
+
+    @Transactional
+    fun modify(workoutLogId: Long, workoutLogDto: WorkoutLogDto) {
+        val workoutLog = WorkoutLog(findOneById(workoutLogId))
+        workoutLog.modify(workoutLogDto)
+    }
+
+    @Transactional(readOnly = true)
+    fun findOneById(workoutLogId: Long): WorkoutLogEntity {
+        return workoutLogRepository.findById(workoutLogId).orElseThrow { WorkoutLogException.NotFoundException() }
+    }
+
 }
